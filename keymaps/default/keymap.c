247ec7d0c2a37e1d7fe4906540becfa5329f3d4b
LEYLA <Leyla@tutamail.com>
2023-08-20 04:09:19 +0100

    That Was a Lie (add NKRO)

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -6,16 +6,16 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
-     * │   │   │ W │   │ A │ Y │ U │ I │ O │← ←│
+     * │   │   │ W │   │ A │ U │ I │ O │ P │BSp│
      * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
-     * │Sft│ A │ S │ D │ D │ H │ J │ K │ L │ent│
+     * │Sft│ A │ S │ D │ D │ J │ K │ L │ ; │Ent│
      * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
      * │   │   │   │Ctl│ W │   │   │   │   │   │
      * └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
      */
     [0] = LAYOUT_ortho_3x10(
         KC_NO,    KC_NO,    KC_W,    KC_NO,    KC_A,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_D,    KC_J,    KC_K,    KC_L,    KC_SCLN,    KC_ENT,
+        KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_D,    KC_J,    KC_K,    KC_L,    KC_SCLN,    KC_ENTER,
         KC_NO,    KC_NO,    KC_NO,    KC_LCTL,    KC_W,    KC_NO,    KC_NO,    KC_NO, KC_NO,  KC_NO
     )
 };
